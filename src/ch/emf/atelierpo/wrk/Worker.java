481abb22659d958497ecf4644df8b6d8edb89bc5
MariusPochon <marius.pochon@studentfr.ch>
2024-04-17 08:58:05 +0200

    javadoc

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -6,7 +6,13 @@ package ch.emf.atelierpo.wrk;
  */
 public class Worker {
 
-   
+    /**
+     * Cette méthode contrôle si la plaque d'immatriculation est correcte.
+     *
+     * @param canton doit etre egal a une abreviation de canton en MAJ uniquement
+     * @param numeroDePlaque doit etre un numero 
+     * @return true si la plaque est correcte, sinon false
+     */
     public boolean controlePlaqueImmatriculation(String canton, String numeroDePlaque) {
         return false;
     }
